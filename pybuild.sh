f6c6046fdcc9c41cd219859debb828e14e11d675
KeepinItRail <dewsoul@pm.me>
2024-06-23 08:54:09 +0100

    add: 9200 Series

--- pybuild.sh
+++ pybuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python nml_patcher.py -f "JPplusTrams.pnml" -o "JPplusTrams.nml" -b 1 -v 1
+python nml_patcher.py -f "JPplusTrams.pnml" -o "JPplusTrams.nml" -b 0 -v 1
 nmlc JPplusTrams.nml -o JPplusTrams.grf -c -t src/custom_tags.txt -l src/lang
 python move_grf.py
